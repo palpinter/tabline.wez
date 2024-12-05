2b2e817e8f279b155fd9b1ce1f4ae3f25bf367a4
Pintér Pál <pal@palpinter.com>
2024-12-05 21:42:19 +0100

    Update tab.lua

--- plugin/tabline/components/tab/tab.lua
+++ plugin/tabline/components/tab/tab.lua
@@ -6,6 +6,6 @@ return {
   },
   update = function(tab)
     local tab_title = tab.tab_title
-    return tab_title == '' and 'default' or tab_title
+    return tab_title == '' and 'default2' or tab_title
   end,
 }
